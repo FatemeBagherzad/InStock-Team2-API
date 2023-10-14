03f351e42db1d1bd66ebea4c4e8e0ca95204a02b
Aung Zaw Myo Naing <sufyan99@gmail.com>
2023-10-14 12:46:00 -0700

    updated get warehuse ID

--- controllers/controllers-warehouses.js
+++ controllers/controllers-warehouses.js
@@ -1,30 +1,47 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
 
 exports.getAllWarehouses = (req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .then((data) => {
       res.status(200).json(data);
-      console.log('In W');
+      console.log("In W");
     })
     .catch((err) => res.status(400).send(`Error retrieving data: ${err}`));
-  console.log('req');
+  console.log("req");
 };
 exports.postNewWarehouse = (req, res) => {
-  console.log('req');
+  console.log("req");
 };
 exports.editWarehouse = (req, res) => {
-  console.log('req');
+  console.log("req");
 };
 exports.deleteWarehouse = (req, res) => {
-  console.log('req');
+  console.log("req");
 };
 
 exports.getWarehouseById = (req, res) => {
-  console.log('req');
+  knex("warehouses")
+    .where({ id: req.params.id })
+    .then((warehouse) => {
+      if (warehouse.length === 0) {
+        return res
+          .status(404)
+          .json({ message: `Warehouse with ID: ${req.params.id} not found` });
+      }
+      const warehouseData = warehouse;
+      console.log(warehouseData);
+      return res.status(200).json(warehouseData);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: `Unable to retrieve warehouse data for warehouse with ID: ${req.params.id}`,
+      });
+    });
+  console.log("req");
 };
 exports.editWarehouseById = (req, res) => {
-  console.log('req');
+  console.log("req");
 };
 exports.edeleteWarehouseById = (req, res) => {
-  console.log('req');
+  console.log("req");
 };
