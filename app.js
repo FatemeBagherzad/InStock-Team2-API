e9547b397ee29466e96bdc8693853b2284e3f498
Aung Zaw Myo Naing <sufyan99@gmail.com>
2023-10-15 05:29:04 -0700

    controllers-warehouse updated

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const routerW = require('./routers/router-warehouses');
-const routerI = require('./routers/router-inventories');
+const express = require("express");
+const cors = require("cors");
+const routerW = require("./routers/router-warehouses");
+const routerI = require("./routers/router-inventories");
+require("dotenv").config();
 
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/dev-data/public')));
+app.use(express.static(path.join(__dirname, "/dev-data/public")));
 
-app.use('/warehouses', routerW);
-app.use('/inventory', routerI);
+app.use("/warehouses", routerW);
+app.use("/inventory", routerI);
 
 module.exports = app;
